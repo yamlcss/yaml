62dd5aca35fde816a8f8d8dbf5acb99de741398a
Michael Schulze <m.schulze@elsigno.de>
2013-03-31 19:25:24 +0200

    missing comma in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
           outputStyle    : 'expanded',
           noLineComments : true
         }
-      }
+      },
       css: {
         options: {
           sassDir        : 'sass/css',
